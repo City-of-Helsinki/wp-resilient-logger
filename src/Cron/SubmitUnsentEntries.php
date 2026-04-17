9bcb6e480a8260b2856e39654f29ed502089fadb
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-17 13:51:09 +0300

    Update SubmitUnsentEntries.php

--- src/Cron/SubmitUnsentEntries.php
+++ src/Cron/SubmitUnsentEntries.php
@@ -4,12 +4,29 @@ declare(strict_types=1);
 
 namespace WP\helfi_resilient_logger\Cron;
 
-class SubmitUnsentEntries {
-    public static function trigger(): void {
+use WP\helfi_resilient_logger\Enums\ResilientLoggerSchedule;
+
+final class SubmitUnsentEntries implements ScheduledAction
+{
+	public function action(): string
+	{
+		return 'resilient_logger.submit_unsent_entries';
+	}
+
+	public function interval(): ResilientLoggerSchedule
+	{
+		return ResilientLoggerSchedule::FIFTEEN_MINUTES;
+	}
+
+    public function trigger(): void
+	{
 		try {
 			\do_action( 'helsinki_wp_resilient_logger_submit_unsent_entries' );
 		} catch (\Exception $e) {
-			error_log(sprintf("ResilientLogger error: %s", $e->getMessage()));
+			error_log( sprintf(
+				'ResilientLogger error: %s',
+				$e->getMessage()
+			) );
 		}
     }
 }
