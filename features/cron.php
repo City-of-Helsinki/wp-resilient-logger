a1f67ef2cfb9dc8a8e4b53eb123672c8dae3f236
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-17 15:30:44 +0300

    Create cron.php

---
+++ features/cron.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+use WP\helfi_resilient_logger\Cron\ResilientLoggerSchedule;
+use WP\helfi_resilient_logger\Helpers\CurrentEnvironment;
+
+\add_action( 'helsinki_wp_resilient_logger_activate', function( CurrentEnvironment $environment ) {
+
+	if ( $environment->use_wp_cron ) {
+		$scheduler = helsinki_wp_resilient_logger_scheduler();
+
+		foreach ( $scheduler->schedules() as $action => $interval ) {
+			if ( ! \wp_next_scheduled( $action ) ) {
+                \wp_schedule_event( time(), $interval, $action );
+            }
+		}
+	}
+
+} );
+
+\add_action( 'helsinki_wp_resilient_logger_deactivate', function() {
+
+	$scheduler = helsinki_wp_resilient_logger_scheduler();
+
+	foreach ( $scheduler->schedules() as $action ) {
+		\wp_clear_scheduled_hook( $action );
+	}
+
+} );
+
+\add_action( 'helsinki_wp_resilient_logger_loaded', function() {
+
+	if ( \apply_filters( 'helsinki_wp_resilient_logger_use_wp_cron', false ) ) {
+		$scheduler = helsinki_wp_resilient_logger_scheduler();
+
+		foreach ( $scheduler->handlers() as $action => $handler ) {
+			\add_action( $action, $handler );
+		}
+	}
+
+	\add_action( 'init', function() {
+		if ( \apply_filters( 'helsinki_wp_resilient_logger_use_wp_cron', false ) ) {
+			\add_filter( 'cron_schedules', 'helsinki_wp_resilient_logger_schedules' );
+		}
+	} );
+
+}, 20 );
+
+function helsinki_wp_resilient_logger_schedules( array $schedules ): array {
+	return array_reduce(
+		ResilientLoggerSchedule::cases(),
+		function( array $schedules, ResilientLoggerSchedule $schedule ) {
+			$schedules[$schedule->value] = array(
+				'interval' => $schedule->interval(),
+				'display'  => $schedule->label(),
+			);
+
+			return $schedules;
+		},
+		$schedules
+	);
+}
