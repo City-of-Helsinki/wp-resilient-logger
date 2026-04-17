e5b3d9d2cebba900370b6b41cfa7e723af6345bd
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-17 13:51:05 +0300

    Create ScheduledAction.php

---
+++ src/Cron/ScheduledAction.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WP\helfi_resilient_logger\Cron;
+
+use WP\helfi_resilient_logger\Enums\ResilientLoggerSchedule;
+
+interface ScheduledAction
+{
+	public function action(): string;
+	public function interval(): ResilientLoggerSchedule;
+    public function trigger(): void;
+}
