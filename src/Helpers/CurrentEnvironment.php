f50a5bab564d57b802a897d7cb857359fd632e60
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-17 14:31:28 +0300

    move CurrentEnvironment

--- src/Helpers/CurrentEnvironment.php
+++ src/Helpers/CurrentEnvironment.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace WP\helfi_resilient_logger;
+namespace WP\helfi_resilient_logger\Helpers;
 
 use WpSecurityAuditLog;
 use WSAL\Controllers\Alert_Manager;
@@ -13,12 +13,14 @@ readonly class CurrentEnvironment
 	public bool $is_cli;
 	public bool $use_wp_cron;
 	public bool $wsal_active;
+	public bool $wsal_enforce_settings;
 
 	public function __construct()
 	{
 		$this->is_debug = $this->determine( 'WP_DEBUG' );
 		$this->is_cli = $this->determine( 'WP_CLI' );
 		$this->use_wp_cron = $this->determine( 'RESILIENT_LOGGER_USE_WP_CRON' );
+		$this->wsal_enforce_settings = $this->determine( 'RESILIENT_LOGGER_WSAL_ENFORCE_SETTINGS' );
 
 		$this->wsal_active = class_exists( WpSecurityAuditLog::class )
 			&& class_exists( Alert_Manager::class );
