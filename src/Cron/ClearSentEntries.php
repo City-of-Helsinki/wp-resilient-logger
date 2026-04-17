494d948269356b2f273ccb04f595f2ebffc06876
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-17 09:54:58 +0300

    Update ClearSentEntries.php

--- src/Cron/ClearSentEntries.php
+++ src/Cron/ClearSentEntries.php
@@ -4,20 +4,12 @@ declare(strict_types=1);
 
 namespace WP\helfi_resilient_logger\Cron;
 
-use WP\helfi_resilient_logger\ResilientLogger;
-
 class ClearSentEntries {
     public static function trigger(): void {
-        $settings = ResilientLogger::getSettings();
-        $enabled = (bool) ($settings['clear_sent_entries'] ?? false);
-        $daysToKeep = (int) ($settings['days_to_keep'] ?? 30);
-
-        if ($enabled) {
-            try {
-                ResilientLogger::getInstance()->clearSentEntries($daysToKeep);
-            } catch (\Exception $e) {
-                error_log(sprintf("ResilientLogger error: %s", $e->getMessage()));
-            }
-        }
+		try {
+			\do_action( 'helsinki_wp_resilient_logger_clear_sent_entries' );
+		} catch (\Exception $e) {
+			error_log(sprintf("ResilientLogger error: %s", $e->getMessage()));
+		}
     }
 }
