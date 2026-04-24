aef10982dc0f1beb9ab95e8b3c3aa3f23c265e6d
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-24 17:33:17 +0300

    Create WSALLogSource.php

---
+++ src/Sources/WSAL/WSALLogSource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CityOfHelsinki\WP\ResilientLogger\Sources\WSAL;
+
+use CityOfHelsinki\WP\ResilientLogger\ResilientLoggerConfig;
+use ResilientLogger\Sources\AbstractLogSourceEntry;
+use ResilientLogger\Sources\AbstractLogSource;
+
+final class WSALLogSource implements AbstractLogSource
+{
+	public function __construct(
+		private WSALData $data,
+		private WSALAlertAdapter $alerts,
+		private ResilientLoggerConfig $config
+	) {}
+
+	public function create(int $level, mixed $message, array $context = []): ?AbstractLogSourceEntry
+	{
+		\do_action(
+			'helsinki_wp_resilient_logger_does_create_entries',
+			get_class( $this )
+		);
+
+		return null;
+	}
+
+	public function getUnsentEntries(int $chunkSize): \Generator
+	{
+		foreach( $this->data->unsent($chunkSize) as $row ) {
+			yield $this->createEntry($row);
+		}
+	}
+
+	public function clearSentEntries(int $daysToKeep): void
+	{
+		\do_action(
+			'helsinki_wp_resilient_logger_does_not_clear_sent_entries',
+			get_class( $this )
+		);
+	}
+
+	private function createEntry(array $row): AbstractLogSourceEntry
+	{
+		return new WSALLogSourceEntry(
+			$this->data,
+			$this->alerts,
+			$this->config,
+			$row
+		);
+	}
+}
