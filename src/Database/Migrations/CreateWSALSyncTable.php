77d0ecfa39a7ab77cf771f32e2c9645e4b04e8cc
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-24 17:30:51 +0300

    Update CreateWSALSyncTable.php

--- src/Database/Migrations/CreateWSALSyncTable.php
+++ src/Database/Migrations/CreateWSALSyncTable.php
@@ -2,20 +2,24 @@
 
 namespace CityOfHelsinki\WP\ResilientLogger\Database\Migrations;
 
+use CityOfHelsinki\WP\ResilientLogger\Database\ResilientLoggerTables;
+use wpdb;
+
 final class CreateWSALSyncTable
 {
 	public function __construct(
-		private string $prefix,
-		private string $charset
+		private wpdb $db
 	) {}
 
 	public function up(): void
 	{
-		\dbDelta("CREATE TABLE {$this->prefix}helfi_wsal_sync (
+		$table = ResilientLoggerTables::wsal_sync($this->db);
+
+		\dbDelta("CREATE TABLE {$table} (
             occurrence_id bigint(20) NOT NULL,
             is_sent tinyint(1) NOT NULL DEFAULT 0,
             sent_at datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
             PRIMARY KEY (occurrence_id)
-        ) {$this->charset};");
+        ) {$this->db->get_charset_collate()};");
 	}
 }
