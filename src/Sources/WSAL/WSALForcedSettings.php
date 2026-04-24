075eeeeadf8c8730b55106c8f307e1d00fc81a52
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-24 17:59:31 +0300

    Create WSALForcedSettings.php

---
+++ src/Sources/WSAL/WSALForcedSettings.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CityOfHelsinki\WP\ResilientLogger\Sources\WSAL;
+
+enum WSALForcedSettings: string
+{
+	case PRUNING_UNIT = 'wsal_pruning-unit';
+	case PRUNING_DATE = 'wsal_pruning-date';
+	case PRUNING_ENABLED = 'wsal_pruning-date-e';
+	// case DISABLED_ALERTS = 'wsal_disabled-alerts';
+
+	public function override(): mixed
+    {
+        return match( $this ) {
+            self::PRUNING_UNIT => 'months',
+            self::PRUNING_DATE => '6 months',
+            self::PRUNING_ENABLED => 'yes',
+            // self::DISABLED_ALERTS => [0, 8825, 8845],
+        };
+    }
+}
