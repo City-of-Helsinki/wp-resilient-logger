2fcdbcd0b48450eeaf4ff73211bceadc75936bd1
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-24 15:27:21 +0300

    Update ResilientLoggerAdapter.php

--- src/ResilientLoggerAdapter.php
+++ src/ResilientLoggerAdapter.php
@@ -20,22 +20,22 @@ final class ResilientLoggerAdapter
 
 	public function submit_unsent_entries(): void
 	{
-		if ( $this->config->bool_setting( 'submit_unsent_entries' ) ) {
+		if ( $this->config->submit_unsent_entries() ) {
 			$results = $this->logger->submitUnsentEntries();
 			$failures = array_filter( $results, fn($success) => !$success );
 
 			if ( $failures ) {
-				throw new ResilientLoggerException::submit_unset_entries_failed( $failures );
+				throw ResilientLoggerException::submit_unset_entries_failed( $failures );
 			}
 		}
 	}
 
 	public function clear_sent_entries(): void
 	{
-		if ( $this->config->bool_setting( 'clear_sent_entries' ) ) {
+		if ( $this->config->clear_sent_entries() ) {
 			$this->logger->clearSentEntries();
 		} else {
-			throw new ResilientLoggerException::clear_sent_entries_disabled();
+			throw ResilientLoggerException::clear_sent_entries_disabled();
 		}
 	}
 }
