ff792bf4be9d35e678bf63dfc492857c6e2604d1
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-05-06 13:38:56 +0300

    Update ResilientLoggerLogSource.php

--- src/Sources/Native/ResilientLoggerLogSource.php
+++ src/Sources/Native/ResilientLoggerLogSource.php
@@ -5,6 +5,7 @@ namespace CityOfHelsinki\WP\ResilientLogger\Sources\Native;
 use CityOfHelsinki\WP\ResilientLogger\ResilientLoggerConfig;
 use ResilientLogger\Sources\AbstractLogSourceEntry;
 use ResilientLogger\Sources\AbstractLogSource;
+use ResilientLogger\Utils\Helpers;
 
 final class ResilientLoggerLogSource implements AbstractLogSource
 {
@@ -34,6 +35,33 @@ final class ResilientLoggerLogSource implements AbstractLogSource
 
 	private function createEntry(array $row): AbstractLogSourceEntry
 	{
-		return new ResilientLoggerSourceEntry($this->data, $this->config, $row);
+		$message = json_decode( $this->row['message'], true ) ?: '';
+		$message = is_array( $message ) ? json_encode( $message ) : (string) $message;
+
+		$context = json_decode($this->row['context'], true) ?: array();
+		$actor     = $context['actor']     ?? 'unknown';
+		$operation = $context['operation'] ?? 'MANUAL';
+		$target    = $context['target']    ?? 'unknown';
+
+		unset( $context['actor'], $context['operation'], $context['target'] );
+
+		$entry = array(
+			'id' => isset( $row['id'] ) ? (int) $row['id'] : 0,
+			'is_sent' => isset( $row['is_sent'] ) ? (bool) $row['is_sent'] : false,
+			'message' => $message,
+			'context' => $context,
+			'created_at' => new \DateTimeImmutable( $row['created_at'] ),
+			'level' => (int) $row['level'],
+			'actor' => Helpers::valueAsArray( $actor ),
+			'operation' => $operation,
+			'target' => Helpers::valueAsArray( $target ),
+			'origin' => $this->config->origin(),
+			'environment' => $this->config->environment(),
+		);
+
+		return new ResilientLoggerSourceEntry(
+			(object) $entry,
+			$this->data,
+		);
 	}
 }
