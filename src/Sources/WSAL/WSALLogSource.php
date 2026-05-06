fec6fad63a499e83818d39ea5c997004e7ea8d56
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-05-06 13:22:46 +0300

    Update WSALLogSource.php

--- src/Sources/WSAL/WSALLogSource.php
+++ src/Sources/WSAL/WSALLogSource.php
@@ -3,6 +3,8 @@
 namespace CityOfHelsinki\WP\ResilientLogger\Sources\WSAL;
 
 use CityOfHelsinki\WP\ResilientLogger\ResilientLoggerConfig;
+use CityOfHelsinki\WP\ResilientLogger\Sources\WSAL\Lookups\AlertDetails;
+use CityOfHelsinki\WP\ResilientLogger\Sources\WSAL\Lookups\AlertTarget;
 use ResilientLogger\Sources\AbstractLogSourceEntry;
 use ResilientLogger\Sources\AbstractLogSource;
 
@@ -10,7 +12,8 @@ final class WSALLogSource implements AbstractLogSource
 {
 	public function __construct(
 		private WSALData $data,
-		private WSALAlertAdapter $alerts,
+		private AlertDetails $details,
+		private AlertTarget $target,
 		private ResilientLoggerConfig $config
 	) {}
 
@@ -41,11 +44,25 @@ final class WSALLogSource implements AbstractLogSource
 
 	private function createEntry(array $row): AbstractLogSourceEntry
 	{
-		return new WSALLogSourceEntry(
-			$this->data,
-			$this->alerts,
-			$this->config,
-			$row
+		$entry = array(
+			'id' => isset( $row['id'] ) ? (int) $row['id'] : 0,
+			'site_id' => $row['site_id'] ?? 1,
+			'alert_id' => (int) $row['alert_id'] ?? 0,
+			'is_sent' => isset( $row['is_sent'] ) ? (bool) $row['is_sent'] : false,
+			'message' => (string) ($row['message'] ?? ''),
+			'meta' => $row['meta_values'] ?? array(),
+			'created_on' => (int) $row['created_on'] ?? time(),
+			'origin' => $this->config->origin(),
+			'environment' => $this->config->environment(),
 		);
+
+		$entry['details'] = $this->details->parse( $entry['alert_id'] );
+
+		$entry['target'] = array_merge(
+			array( 'site_id' => $entry['site_id'] ),
+			$this->target->parse( $entry['meta'], $entry['alert_id'] )
+		);
+
+		return new WSALLogSourceEntry( (object) $entry, $this->data );
 	}
 }
