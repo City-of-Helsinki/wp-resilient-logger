81621a32044e98ca9df39a744546c834d7609bb6
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-06-05 12:20:14 +0300

    Update wsal.php

--- features/wsal.php
+++ features/wsal.php
@@ -10,53 +10,73 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	if ( \apply_filters( 'helsinki_wp_resilient_logger_wsal_active', false ) ) {
 
-		if ( \apply_filters( 'helsinki_wp_resilient_logger_wsal_enforce_settings', false ) ) {
+		$settings = helsinki_wp_resilient_logger_wsal_settings_hooks();
+
+		if ( \apply_filters( 'helsinki_wp_resilient_logger_wsal_disallow_edit_settings', false ) ) {
 			\add_filter(
 				'wsal_user_can',
-				function( bool $result, mixed $user, string $action ): bool {
-					return 'edit' === $action ? false : $result;
-				},
+				array( $settings, 'disable_editing' ),
 				PHP_INT_MAX, 3
 			);
 
 			\add_filter(
-				'bulk_actions-toplevel_page_wsal-auditlog',
-				'__return_empty_array'
+				'wsal_skip_views',
+				array( $settings, 'disable_settings_view' )
+			);
+		} else {
+			\add_filter(
+				'in_admin_header',
+				array( $settings, 'settings_notice' ),
+				100
 			);
 
-			\add_filter( 'wsal_skip_views', function( array $views ): array {
-				return array_merge( $views, array(
-					'WSAL_Views_Help',
-					'WSAL_Views_Settings',
-					'WSAL_Views_ToggleAlerts',
-					'\WSAL\Views\Premium_Features',
-				) );
-			} );
-
-			$settings = helsinki_wp_resilient_logger_wsal_settings_hooks();
-
-			foreach( $settings->overrides() as $pre_option => $override ) {
-				\add_filter( $pre_option, fn() => $override, PHP_INT_MAX, 1 );
-			}
-
-			\add_action( 'wp_ajax_wsal_reset_settings', function() {
-				\wp_send_json_error(
-					\esc_html( _x( 'How about no', 'WSAL disabled ajax message', 'wp-resilient-logger' ) )
-				);
-			}, 1 );
-
-			\add_action( 'wp_ajax_wsal_purge_activity', function() {
-				\wp_send_json_error(
-					\esc_html( _x( 'How about no', 'WSAL disabled ajax message', 'wp-resilient-logger' ) )
-				);
-			}, 1 );
+			\add_filter(
+				'gettext_wp-security-audit-log',
+				array( $settings, 'forced_setting_text' ),
+				10, 2
+			);
 		}
 
-		\add_action( 'admin_menu', function(): void {
-			\remove_submenu_page( 'wsal-auditlog', 'upgrade' );
-		}, 999 );
+		foreach( $settings->overrides() as $pre_option => $override ) {
+			\add_filter( $pre_option, fn() => $override, PHP_INT_MAX, 1 );
+		}
 
 		$hooks = helsinki_wp_resilient_logger_wsal_hooks();
+
+		\add_filter(
+			'admin_menu',
+			array( $hooks, 'disable_menu_ad' ),
+			999
+		);
+
+		\add_filter(
+			'in_admin_footer',
+			array( $hooks, 'disable_footer_ad' ),
+			-1
+		);
+
+		\add_filter(
+			'wsal_skip_views',
+			array( $hooks, 'skipped_views' )
+		);
+
+		if ( \apply_filters( 'helsinki_wp_resilient_logger_wsal_disable_events_view', false ) ) {
+			\add_filter(
+				'wsal_skip_views',
+				array( $hooks, 'disable_events_view' )
+			);
+		} else {
+			\add_filter(
+				'in_admin_header',
+				array( $hooks, 'events_notice' ),
+				100
+			);
+		}
+
+		\add_filter(
+			'bulk_actions-toplevel_page_wsal-auditlog',
+			array( $hooks, 'disable_bulk_actions' )
+		);
 
 		\add_action(
 			'pre_post_update',
@@ -68,6 +88,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 			array( $hooks, 'augment_event_data' ),
 			PHP_INT_MAX
 		);
-	}
 
+		\add_action(
+			'wp_ajax_wsal_reset_settings',
+			array( $hooks, 'disable_ajax_actions' ),
+			1
+		);
+
+		\add_action(
+			'wp_ajax_wsal_purge_activity',
+			array( $hooks, 'disable_ajax_actions' ),
+			1
+		);
+	}
 }, 30 );
