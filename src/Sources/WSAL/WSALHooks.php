79440e59c599cdc0efdc314f95f8b5c96a9b5b9f
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-06-05 12:19:55 +0300

    Update WSALHooks.php

--- src/Sources/WSAL/WSALHooks.php
+++ src/Sources/WSAL/WSALHooks.php
@@ -10,11 +10,67 @@ final class WSALHooks
 {
 	/** Stores previous post contents here for diff as array [postId => postContents] */
 	private array $post_contents;
+	private bool $is_premium;
 
 	public function __construct(
 		private HumanReadableDiffer $differ
 	) {
 		$this->post_contents = array();
+
+		$this->is_premium = function_exists( 'wsal_freemius' )
+			&& wsal_freemius()?->is_premium();
+	}
+
+	public function disable_ajax_actions(): void
+	{
+		\wp_send_json_error(
+			\esc_html( _x( 'You are not allowed to reset or purge Activity Log data.', 'WSAL disabled ajax message', 'wp-resilient-logger' ) )
+		);
+	}
+
+	public function disable_bulk_actions(): array
+	{
+		return array();
+	}
+
+	public function disable_footer_ad(): void
+	{
+		\remove_action( 'in_admin_footer', array( 'WSAL_AbstractView', 'in_admin_footer' ) );
+	}
+
+	public function disable_menu_ad(): void
+	{
+		\remove_submenu_page( 'wsal-auditlog', 'upgrade' );
+	}
+
+	public function skipped_views( array $views ): array
+	{
+		$skip = array( 'WSAL_Views_Help' );
+
+		if ( ! $this->is_premium ) {
+			$skip[] = '\WSAL\Views\Premium_Features';
+		}
+
+		return array_merge( $views, $skip );
+	}
+
+	public function disable_events_view( array $views ): array
+	{
+		return array_merge( $views, array( 'WSAL_Views_ToggleAlerts' ) );
+	}
+
+	public function events_notice(): void
+	{
+		global $current_screen;
+
+		if ( 'wp-activity-log_page_wsal-togglealerts' === $current_screen?->base ) {
+			printf(
+				'<div class="notice notice-warning">
+					<p>%s</p>
+				</div>',
+				\esc_html( __( 'When using Helfi Resilient Logger plugin all Activity Log events are logged by default and any changes made to the events list are ignored.', 'wp-resilient-logger' ) )
+			);
+		}
 	}
 
 	public function capture_old_post_content( int $post_id ): void
