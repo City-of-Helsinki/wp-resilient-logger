d537e627946be027cdd5c947956e5ef6fa05970c
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-06-08 16:20:51 +0300

    transient cron exists checker for dashboard

--- features/cron.php
+++ features/cron.php
@@ -10,26 +10,13 @@ use CityOfHelsinki\WP\ResilientLogger\Cron\ResilientLoggerSchedule;
 
 \add_action( 'helsinki_wp_resilient_logger_activate', function() {
 
-	if ( \apply_filters( 'helsinki_wp_resilient_logger_use_wp_cron', false ) ) {
-
-		$scheduler = helsinki_wp_resilient_logger_scheduler();
-
-		foreach ( $scheduler->schedules() as $action => $interval ) {
-			if ( ! \wp_next_scheduled( $action ) ) {
-                \wp_schedule_event( time(), $interval, $action );
-            }
-		}
-	}
+	helsinki_wp_resilient_logger_setup_schedules();
 
 } );
 
 \add_action( 'helsinki_wp_resilient_logger_deactivate', function() {
 
-	$scheduler = helsinki_wp_resilient_logger_scheduler();
-
-	foreach ( $scheduler->schedules() as $action ) {
-		\wp_clear_scheduled_hook( $action );
-	}
+	helsinki_wp_resilient_logger_clear_schedules();
 
 } );
 
@@ -43,6 +30,12 @@ use CityOfHelsinki\WP\ResilientLogger\Cron\ResilientLoggerSchedule;
 		}
 	}
 
+	\add_action( 'current_screen', function( WP_Screen $screen ) {
+		if ( 'dashboard' === $screen->base ) {
+			helsinki_wp_resilient_logger_ensure_schedules_exist();
+		}
+	} );
+
 	\add_action( 'init', function() {
 		if ( \apply_filters( 'helsinki_wp_resilient_logger_use_wp_cron', false ) ) {
 			\add_filter( 'cron_schedules', 'helsinki_wp_resilient_logger_schedules' );
@@ -50,6 +43,36 @@ use CityOfHelsinki\WP\ResilientLogger\Cron\ResilientLoggerSchedule;
 	} );
 
 }, 20 );
+
+function helsinki_wp_resilient_logger_ensure_schedules_exist(): void {
+	$transient = 'helsinki_wp_resilient_logger_schedules_exist';
+
+	if ( ! \get_transient( $transient ) ) {
+		helsinki_wp_resilient_logger_setup_schedules();
+
+		\set_transient( $transient, 1, DAY_IN_SECONDS );
+	}
+}
+
+function helsinki_wp_resilient_logger_setup_schedules(): void {
+	if ( \apply_filters( 'helsinki_wp_resilient_logger_use_wp_cron', false ) ) {
+		$scheduler = helsinki_wp_resilient_logger_scheduler();
+
+		foreach ( $scheduler->schedules() as $action => $interval ) {
+			if ( ! \wp_next_scheduled( $action ) ) {
+                \wp_schedule_event( time(), $interval, $action );
+            }
+		}
+	}
+}
+
+function helsinki_wp_resilient_logger_clear_schedules(): void {
+	$scheduler = helsinki_wp_resilient_logger_scheduler();
+
+	foreach ( $scheduler->schedules() as $action ) {
+		\wp_clear_scheduled_hook( $action );
+	}
+}
 
 function helsinki_wp_resilient_logger_schedules( array $schedules ): array {
 	return array_reduce(
