2b457004b659c705b8235c11e1b08ddaad8e8815
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-06-12 16:35:36 +0300

    add logging gates

---
+++ src/Sources/WSAL/Gates/LoginLogout.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CityOfHelsinki\WP\ResilientLogger\Sources\WSAL\Gates;
+
+use CityOfHelsinki\WP\ResilientLogger\Sources\Gates\EventGate;
+
+final class LoginLogout implements EventGate
+{
+	public function should_log( int $event_id, array $data ): bool
+	{
+		return match( $event_id ) {
+			1000 => $this->log_action( 'login', $data ),
+			1001 => $this->log_action( 'logout', $data ),
+			default => true,
+		};
+	}
+
+	private function log_action( string $action, array $data ): bool
+	{
+		foreach( $this->user_roles( $data ) as $role ) {
+			if ( ! $this->determine_logging( $role, $action ) ) {
+				return false;
+			}
+		}
+
+		return $this->determine_logging( $action );
+	}
+
+	private function determine_logging( string ...$parts ): bool
+	{
+		return \apply_filters(
+			sprintf( 'helsinki_wp_resilient_logger_log_%s', implode( '_', $parts ) ),
+			true
+		);
+	}
+
+	private function user_roles( array $data ): array
+	{
+		if (
+			isset( $data['CurrentUserRoles'] )
+			&& is_array( $data['CurrentUserRoles'] )
+		) {
+			return $data['CurrentUserRoles'];
+		}
+
+		return array();
+	}
+}
