45164529e2ee03a21a475a53bc02dc053da0d841
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-06-18 13:58:37 +0300

    disable available update wsal alerts

--- src/Sources/WSAL/Settings/WSALDisabledAlerts.php
+++ src/Sources/WSAL/Settings/WSALDisabledAlerts.php
@@ -6,7 +6,7 @@ namespace CityOfHelsinki\WP\ResilientLogger\Sources\WSAL\Settings;
 
 enum WSALDisabledAlerts: int
 {
-	// WordPress
+	// Content
 	case VIEW_POST_EDIT = 2100;
 	case VIEW_POST = 2101;
 	case VIEW_PAGE_EDIT = 2102;
@@ -14,6 +14,11 @@ enum WSALDisabledAlerts: int
 	case VIEW_CPT_EDIT = 2104;
 	case VIEW_CPT = 2105;
 	case VIEW_PROTECTED_POST = 2134;
+
+	// Updates
+	case AVAILABLE_PLUGIN_UPDATE = 5032;
+	case AVAILABLE_THEME_UPDATE = 5033;
+	case AVAILABLE_CORE_UPDATE = 6079;
 
 	// Cron
 	case RECURRING_TASK_EXECUTED = 6070;
